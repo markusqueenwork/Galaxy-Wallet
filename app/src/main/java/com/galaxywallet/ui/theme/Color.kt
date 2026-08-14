f14a37791bc21d59c338f32455af27f65236a640
markusqueenwork <markusqueenwork@gmail.com>
2026-08-14 05:48:08 +0300

    Create Color.kt

---
+++ app/src/main/java/com/galaxywallet/ui/theme/Color.kt
@@ -0,0 +1,25 @@
+package com.galaxywallet.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Phantom-like colors
+val Background = Color(0xFF000000)
+val Surface = Color(0xFF1C1C1E)
+val SurfaceHover = Color(0xFF2C2C2E)
+val TextMain = Color(0xFFFFFFFF)
+val TextSecondary = Color(0xFF8E8E93)
+val Accent = Color(0xFFB39DDB)
+val AccentDark = Color(0xFF6D28D9)
+val Green = Color(0xFF34C759)
+val Red = Color(0xFFFF3B30)
+
+// Coin colors
+val BitcoinOrange = Color(0xFFF7931A)
+val EthereumBlue = Color(0xFF627EEA)
+val SolanaGreen = Color(0xFF14F195)
+val SolanaPurple = Color(0xFF9945FF)
+val TronRed = Color(0xFFFF060A)
+val TonBlue = Color(0xFF0098EA)
+val SuiBlue = Color(0xFF4DA2FF)
+val UsdcBlue = Color(0xFF2775CA)
+val UsdtGreen = Color(0xFF26A17B)
